f8ccd0fe3dec5e3b2462bf07cb9d224412a53d26
89Mods <luca.horn@gmx.de>
2020-02-17 02:42:20 +0100

    Extend type converter and add mergesort program

--- src/theGhastModding/scrapCPU/simulator/TypeConverter.java
+++ src/theGhastModding/scrapCPU/simulator/TypeConverter.java
@@ -19,13 +19,24 @@ public class TypeConverter extends JFrame {
 	private JTextField textField_1;
 	private JTextField textField_2;
 	private JTextField textField_3;
+	private JTextField textField_4;
+	private JTextField textField_5;
+	private JTextField textField_6;
+	private JTextField textField_7;
+	private JLabel lblW_1;
+	private JLabel lblW_2;
+	private JLabel lblW_3;
+	private JLabel lblLongWord;
+	private JTextField textField_8;
+	private JLabel lblFixedPoint_1;
+	private JTextField textField_9;
 
 	public TypeConverter(JComponent parent) {
 		super("Type Converter");
 		this.setResizable(false);
 		this.setDefaultCloseOperation(JFrame.HIDE_ON_CLOSE);
 		this.getContentPane().setLayout(null);
-		this.getContentPane().setPreferredSize(new Dimension(256, 185));
+		this.getContentPane().setPreferredSize(new Dimension(517, 185));
 		
 		JLabel lblSingleWord = new JLabel("Low Word");
 		lblSingleWord.setBounds(12, 12, 111, 15);
@@ -127,7 +138,6 @@ public class TypeConverter extends JFrame {
 		textField_3.setBounds(12, 155, 234, 19);
 		getContentPane().add(textField_3);
 		textField_3.setColumns(10);
-		this.setLocationRelativeTo(parent);
 		
 		textField_3.addKeyListener(new KeyListener() {
 			
@@ -141,13 +151,181 @@ public class TypeConverter extends JFrame {
 						return;
 					}
 					if(floatVal < 0) return;
-					int wordVal = (int)(floatVal * 64.0f) & 0b111111111111;
+					int wordVal = (int)Math.round(floatVal * 64.0f) & 0b111111111111;
 					int word1 = wordVal & 0b00111111;
 					int word2 = (wordVal >> 6) & 0b00111111;
 					textField.setText(Integer.toString(word1));
 					textField_1.setText(Integer.toString(word2));
 					textField_2.setText(Integer.toString(wordVal));
 					textField_3.setText(Float.toString(wordVal / 64.0f));
+				}
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent arg0) {}
+			
+			@Override
+			public void keyTyped(KeyEvent arg0) {}
+			
+		});
+		
+		textField_4 = new JTextField();
+		textField_4.setText("0");
+		textField_4.setColumns(10);
+		textField_4.setBounds(271, 39, 49, 19);
+		getContentPane().add(textField_4);
+		
+		textField_5 = new JTextField();
+		textField_5.setText("0");
+		textField_5.setColumns(10);
+		textField_5.setBounds(332, 39, 50, 19);
+		getContentPane().add(textField_5);
+		
+		textField_6 = new JTextField();
+		textField_6.setText("0");
+		textField_6.setColumns(10);
+		textField_6.setBounds(394, 39, 50, 19);
+		getContentPane().add(textField_6);
+		
+		textField_7 = new JTextField();
+		textField_7.setText("0");
+		textField_7.setColumns(10);
+		textField_7.setBounds(456, 39, 49, 19);
+		getContentPane().add(textField_7);
+		
+		JLabel lblW = new JLabel("W1");
+		lblW.setBounds(271, 12, 49, 15);
+		getContentPane().add(lblW);
+		
+		lblW_1 = new JLabel("W2");
+		lblW_1.setBounds(332, 12, 41, 15);
+		getContentPane().add(lblW_1);
+		
+		lblW_2 = new JLabel("W3");
+		lblW_2.setBounds(394, 12, 41, 15);
+		getContentPane().add(lblW_2);
+		
+		lblW_3 = new JLabel("W4");
+		lblW_3.setBounds(456, 12, 49, 15);
+		getContentPane().add(lblW_3);
+		
+		lblLongWord = new JLabel("Long Word");
+		lblLongWord.setBounds(271, 70, 234, 15);
+		getContentPane().add(lblLongWord);
+		
+		textField_8 = new JTextField();
+		textField_8.setText("0");
+		textField_8.setColumns(10);
+		textField_8.setBounds(271, 97, 234, 19);
+		getContentPane().add(textField_8);
+		
+		lblFixedPoint_1 = new JLabel("Fixed point");
+		lblFixedPoint_1.setBounds(271, 128, 234, 15);
+		getContentPane().add(lblFixedPoint_1);
+		
+		textField_9 = new JTextField();
+		textField_9.setText("0");
+		textField_9.setColumns(10);
+		textField_9.setBounds(271, 155, 234, 19);
+		getContentPane().add(textField_9);
+		this.setLocationRelativeTo(parent);
+		
+		KeyListener al = new KeyListener() {
+			
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				if(arg0.getKeyCode() == KeyEvent.VK_ENTER) {
+					int wordVal1,wordVal2,wordVal3,wordVal4;
+					try {
+						wordVal1 = Integer.parseInt(textField_4.getText().toString());
+						wordVal2 = Integer.parseInt(textField_5.getText().toString());
+						wordVal3 = Integer.parseInt(textField_6.getText().toString());
+						wordVal4 = Integer.parseInt(textField_7.getText().toString());
+					}catch(NumberFormatException e) {
+						return;
+					}
+					wordVal1 &= 0b111111;
+					textField_4.setText(Integer.toString(wordVal1));
+					wordVal2 &= 0b111111;
+					textField_5.setText(Integer.toString(wordVal2));
+					wordVal3 &= 0b111111;
+					textField_6.setText(Integer.toString(wordVal3));
+					wordVal4 &= 0b111111;
+					textField_7.setText(Integer.toString(wordVal4));
+					int wordVal = wordVal1 | (wordVal2 << 6) | (wordVal3 << 12) | (wordVal4 << 18);
+					textField_8.setText(Integer.toString(wordVal));
+					textField_9.setText(Float.toString(wordVal / 4096.0f));
+				}
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent arg0) {}
+			
+			@Override
+			public void keyTyped(KeyEvent arg0) {}
+			
+		};
+		textField_4.addKeyListener(al);;
+		textField_5.addKeyListener(al);
+		textField_6.addKeyListener(al);
+		textField_7.addKeyListener(al);
+		
+		textField_8.addKeyListener(new KeyListener() {
+
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				if(arg0.getKeyCode() == KeyEvent.VK_ENTER) {
+					int wordVal;
+					try {
+						wordVal = Integer.parseInt(textField_8.getText());
+					}catch(NumberFormatException e) {
+						return;
+					}
+					wordVal &= 0b111111111111111111111111;
+					textField_8.setText(Integer.toString(wordVal));
+					int wordVal1 = (wordVal >> 0) & 0b111111;
+					int wordVal2 = (wordVal >> 6) & 0b111111;
+					int wordVal3 = (wordVal >> 12) & 0b111111;
+					int wordVal4 = (wordVal >> 18) & 0b111111;
+					textField_4.setText(Integer.toString(wordVal1));
+					textField_5.setText(Integer.toString(wordVal2));
+					textField_6.setText(Integer.toString(wordVal3));
+					textField_7.setText(Integer.toString(wordVal4));
+					textField_9.setText(Float.toString(wordVal / 4096.0f));
+				}
+			}
+			
+			@Override
+			public void keyReleased(KeyEvent arg0) {}
+			
+			@Override
+			public void keyTyped(KeyEvent arg0) {}
+			
+		});
+		
+		textField_9.addKeyListener(new KeyListener() {
+			
+			@Override
+			public void keyPressed(KeyEvent arg0) {
+				if(arg0.getKeyCode() == KeyEvent.VK_ENTER) {
+					float floatVal;
+					try {
+						floatVal = Float.parseFloat(textField_9.getText());
+					}catch(NumberFormatException e) {
+						return;
+					}
+					if(floatVal < 0) return;
+					int wordVal = (int)Math.round(floatVal * 4096.0f) & 0b111111111111111111111111;
+					textField_9.setText(Float.toString(wordVal / 4096.0f));
+					int wordVal1 = (wordVal >> 0) & 0b111111;
+					int wordVal2 = (wordVal >> 6) & 0b111111;
+					int wordVal3 = (wordVal >> 12) & 0b111111;
+					int wordVal4 = (wordVal >> 18) & 0b111111;
+					textField_4.setText(Integer.toString(wordVal1));
+					textField_5.setText(Integer.toString(wordVal2));
+					textField_6.setText(Integer.toString(wordVal3));
+					textField_7.setText(Integer.toString(wordVal4));
+					textField_8.setText(Integer.toString(wordVal));
 				}
 			}
 			
